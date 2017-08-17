4a02b5265356a1fed54d8afde0ee7bbc287a4de2
Carlos Gonzalez <carlosegonzax@gmail.com>
2017-08-16 20:37:14 -0400

    first commit

---
+++ indexa.php
@@ -0,0 +1,9 @@
+<?php
+
+var_dump(class_exists('PDO'));
+// Or
+var_dump(extension_loaded ('PDO' )); // returns boolean
+// Or
+var_dump(extension_loaded('pdo_mysql'));
+
+?>
---
+++ resources/views/products/fragment/form.blade.php
@@ -0,0 +1,15 @@
+<div class="form-group">
+	{!! Form::label('name', 'Nombre del producto') !!}
+	{!! Form::text('name', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+	{!! Form::label('short', 'Descripcion brebe del producto') !!}
+	{!! Form::text('short', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+	{!! Form::label('body', 'Body del producto') !!}
+	{!! Form::text('body', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+	{!! Form::submit('ENVIAR', ['class' => 'btn btn-primary']) !!}
+</div>
