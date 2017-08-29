24346aa661037458a1666b96f7c42691b4d9aa58
Carlos Gonzalez <carlosegonzax@gmail.com>
2017-08-29 14:14:41 -0400

    redirect to public folder from index

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*var_dump(class_exists('PDO'));
+// Or
+var_dump(extension_loaded ('PDO' )); // returns boolean
+// Or
+var_dump(extension_loaded('pdo_mysql'));*/
+
+header('location: public/index.php');
+
+?>
